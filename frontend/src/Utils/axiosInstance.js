6b5ce0958b68c51504fe77b66e168a35cfd4f23d
het <kalriyahet@gmail.com>
2025-03-24 23:35:21 +0530

    Render Hosting

--- frontend/src/Utils/axiosInstance.js
+++ frontend/src/Utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apipath";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL ,
+  baseURL: BASE_URL || "http://localhost:8000",
   timeout: 10000,
   headers: { "Content-Type": "application/json", Accept: "application/json" },
 });
