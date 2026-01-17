7dabf700c0109fbb6b79b3ecf34748398116445a
Harshal Lambe <hdlambe10@gmail.com>
2026-01-17 22:15:32 +0530

    for backend origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://expense-tracker-1-1hwb.onrender.com", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
